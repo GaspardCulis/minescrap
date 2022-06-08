2dc56b43161777b1e9175beed6d89044ba665a8c
Gaspard Culis <gaspard.culis@gmail.com>
2022-06-08 17:00:51 +0200

    Totally changed code

--- search.js
+++ search.js
@@ -3,7 +3,6 @@ const mcping = require('mc-ping-updated');
 const cliProgress = require('cli-progress');
 fs = require('fs');
 
-
 var active_queries = 0;
 const MAX_QUERIES = 10000;
 
@@ -23,7 +22,6 @@ function newFind(IP, callback=(response, err)=>{}, PORT=25565, auto_restart=true
         active_queries -= 1;
         if (active_queries < MAX_QUERIES & auto_restart) {
             newFind(Utils.generateIp());
-            console.log('Restarting query...');
         }
 
     }, 1000)
@@ -84,54 +82,50 @@ function storeServer(IP, query, callback=(response)=>{}) {
     let response = {
         new_players: 0
     };
-    Utils.findServer(IP, (out, index) => {
-        if (out) {
-            // Updating server in DB
-            out.lastTimeOnline = timestamp;
-            if (query.players.sample) {
-                for(let player of query.players.sample) {
-                    let playerIndex = -1;
-                    for(let i=0; i<out.players.length; i++) {
-                        if (out.players[i].name==player.name) {
-                            playerIndex = i;
-                            break;
-                        }
-                    }
-                    if (playerIndex>=0) {
-                        out.players[playerIndex].lastTimeOnline = timestamp
-                    } else {
-                        let newPlayer = player;
-                        newPlayer.lastTimeOnline = timestamp;
-                        out.players.push(newPlayer);
-                        response.new_players += 1;
-                    }
+    
+    if (IP in database.servers) {
+        // Updating server in DB
+        database.servers[IP].lastTimeOnline = timestamp;
+        if (query.players.sample) {
+            let players = database.servers[IP].players
+            for(let player of query.players.sample) {
+                let playerIndex = 0;
+                while (playerIndex<players.length & 
+                        players[playerIndex].name!=player.name) {
+                    playerIndex+=1;
                 }
-            }
-            database.servers[index] = out;
-        } else {
-            // Adding new server to DB
-            let players = [];
-            if (query.players.sample) {
-                for(let player of query.players.sample) {
+                if (playerIndex<players.length) {
+                    players[playerIndex].lastTimeOnline = timestamp
+                } else {
                     let newPlayer = player;
                     newPlayer.lastTimeOnline = timestamp;
                     players.push(newPlayer);
+                    response.new_players += 1;
                 }
             }
-            let newElement = {
-                "IP":IP,
-                "description":query.description.text,
-                "version":query.version.name,
-                "players":players,
-                "discovered":timestamp,
-                "lastTimeOnline":timestamp
-            }
-            database.servers.push(newElement);
-            console.log(IP+' in '+query.version.name+' new server stored')
         }
-        Utils.storeDatabase(database);
-        callback(response);
-    })
+    } else {
+        // Adding new server to DB
+        let players = [];
+        if (query.players.sample) {
+            for(let player of query.players.sample) {
+                let newPlayer = player;
+                newPlayer.lastTimeOnline = timestamp;
+                players.push(newPlayer);
+            }
+        }
+        let newElement = {
+            "description":query.description.text,
+            "version":query.version.name,
+            "players":players,
+            "discovered":timestamp,
+            "lastTimeOnline":timestamp
+        }
+        database.servers[IP] = newElement;
+        console.log(IP+' in '+query.version.name+' new server stored')
+    }
+    Utils.storeDatabase(database);
+    callback(response);
 }
 
 /*
@@ -141,5 +135,5 @@ for (let i=0; i<MAX_QUERIES; i++) {
 
 
 
-updateServers()
-//searchServers()
+//updateServers()
+searchServers()
