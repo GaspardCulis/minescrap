4f114aee70488a2401af0688bc3a166b20189a89
Gaspard Culis <gaspard.culis@gmail.com>
2022-10-08 00:41:25 +0200

    Added api performance info

--- src/api.js
+++ src/api.js
@@ -7,18 +7,22 @@ const app = express()
 app.set('json spaces', 2)
 
 app.get('/servers', async (req, res) => {
-    console.log(req.query);
+    let t0 = Date.now();
     let servers = await database.getServers(req.query);
     res.json(servers);
+    console.log(`[GET] /servers?${JSON.stringify(req.query)} took ${Date.now() - t0}ms`);
 })
 
 app.get('/players', async (req, res) => {
+    let t0 = Date.now();
     let players = await database.getPlayers(req.query);
     res.json(players);
+    console.log(`[GET] /players took ${Date.now() - t0}ms`);
 })
 
 
 app.get('/', async (req, res) => {
+    let t0 = Date.now();
     res.send(`
         <h2>Welcome to minescrap API !</h2><br><br>
         We currently own <strong>${await database.getServerCount()}</strong> servers and know <strong>${await database.getPlayerCount()}</strong> players.
@@ -51,6 +55,7 @@ app.get('/', async (req, res) => {
         </ul>
 
     `)
+    console.log(`[GET] / took ${Date.now() - t0}ms`);
 })
 
 
