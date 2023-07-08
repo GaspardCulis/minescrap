d5fc5532f428c698d7e32541b480d78dc43895c4
Gaspard Culis <gaspard.culis@gmail.com>
2023-07-08 13:41:13 +0200

    Test push

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://minescrap.github.io',
   integrations: [tailwind(), svelte()]
