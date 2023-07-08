e3a6e66568c63465e62e711e14cd8a5cf28cdada
Gaspard Culis <gaspard.culis@gmail.com>
2023-07-08 13:34:45 +0200

    Added GH pages deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://minescrap.github.io',
   integrations: [tailwind(), svelte()]
 });
