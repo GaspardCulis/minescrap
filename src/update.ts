782397e7d1b2d878f9acb6948f48fda1f9c7ce0d
Gaspard Culis <gaspard.culis@gmail.com>
2023-07-16 11:22:02 +0200

    Added CLI args

--- src/update.ts
+++ src/update.ts
@@ -2,8 +2,28 @@ import { createClient } from "@supabase/supabase-js";
 import { Database } from "./types/supabase";
 import { quickConnect, connectSession } from "./quick-connect";
 import dotenv from "dotenv";
+import yargs from "yargs/yargs";
 
 dotenv.config();
+
+const argv = yargs(process.argv.slice(2))
+    .option("batch-size", {
+        alias: "b",
+        description: "The simultaneous count of servers to check, the higher the faster but more false negatives due to connection errors",
+        type: "number",
+        default: 10
+    })
+    .option("verbose", {
+        alias: "v",
+        description: "Verbose events",
+        type: "boolean",
+    })
+    .help()
+    .alias("help", "h")
+    .parseSync();
+
+const BATCH_SIZE = argv.batchSize;
+const VERBOSE = argv.verbose;
 
 const client = createClient<Database>(
     process.env.SUPABASE_URL!,
@@ -25,7 +45,7 @@ async function* iterateServerBatch(batch_size: number) {
 
 async function main() {
     await connectSession();
-    for await (const batch of iterateServerBatch(10)) {
+    for await (const batch of iterateServerBatch(BATCH_SIZE)) {
         // Run quick connect on each server in the batch
         const results = await Promise.all(
             batch.data!.map(async (server) => {
@@ -39,7 +59,7 @@ async function main() {
         await Promise.all(
             results.map(async (result) => {
                 // Some logging
-                if (result.online_mode !== null && !result.whitelist) {
+                if (result.online_mode !== null && !result.whitelist && VERBOSE) {
                     console.log(`Managed to connect to ${result.ip} with online mode ${result.online_mode}`);
                 }
                 // Update the database
